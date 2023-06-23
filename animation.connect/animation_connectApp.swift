0a11bd363442c031b9c65f6e38d026472b81d39b
Rohit Saini (IND) <rsaini@mafcarrefour.com>
2023-06-23 14:54:12 +0530

    Initial Commit

---
+++ animation.connect/animation_connectApp.swift
@@ -0,0 +1,17 @@
+//
+//  animation_connectApp.swift
+//  animation.connect
+//
+//  Created by Rohit Saini on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct animation_connectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
